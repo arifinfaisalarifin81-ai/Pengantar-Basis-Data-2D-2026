34153033ffc3e8e2502abb7587e6ab57a0287cf0
faisal <arifinfaisalarifin81@gmail.com>
2026-04-17 09:50:46 +0700

    250441100021_Muhammad Faisal Arifin_Modul1_Ahmad Rikhan Arbai'i

---
+++ modul-1/250441100021_Muhammad Faisal Arifin/soal1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE koleksi_langka (
+id_buku BIGINT PRIMARY KEY,
+judul VARCHAR(250),
+tahun_terbit YEAR,
+estimasi_harga DECIMAL(15,2)
+);
---
+++ modul-1/250441100021_Muhammad Faisal Arifin/soal3.sql
@@ -0,0 +1,5 @@
+CREATE TABLE transaksi_harian (
+id_transaksi INT,
+total INT
+);
+TRUNCATE TABLE transaksi_harian;
